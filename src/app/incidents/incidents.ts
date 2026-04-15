6647360ff3919dc6be31dc43969a6741ff7d1b17
Hampter-0 <stijn09dijkhuis@gmail.com>
2026-04-15 19:40:56 +0200

    added incidents page styling, state management, and date switch buttons for future date navigation

--- src/app/incidents/incidents.ts
+++ src/app/incidents/incidents.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-incidents',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './incidents.html',
   styleUrl: './incidents.css',
 })
-export class Incidents {}
+export class Incidents {
+  constructor(private router: Router) {
+
+  }
+}
